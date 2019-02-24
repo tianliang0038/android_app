a97f109e61350eef5ab2695f9108e8dc6ac38f88
YidingOu <yidingou@126.com>
2019-02-24 07:54:37 -0500

    first page

---
+++ app/src/main/java/com/corse_proj/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.corse_proj.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_workout:
+                    mTextMessage.setText("Workout");
+                    return true;
+                case R.id.navigation_challenge:
+                    mTextMessage.setText("Challenge");
+                    return true;
+                case R.id.navigation_account:
+                    mTextMessage.setText("Account");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
